b7d4257f08bae9ab2a164ebd8571821bb04e125f
PaoloL <paolo.latella@xpeppers.com>
2014-12-29 12:56:45 +0100

    Delete old example files

--- swf_starter.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'swf_utils'
-require_relative 'swf_workflow'
-
-#Start a Workflow execution SwfWorkflow and passing argument to Workflow
-SwfUtils.new.workflow_client(SwfWorkflow).start_execution("Demo AWS Flow Framework")
